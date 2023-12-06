bd81b0a9fd26a1e04de3241fe247bc6fa6c199c1
webwizardsusa <webwizardsusa@WWU.local>
2023-12-06 13:03:05 +0530

    Laravel Socialite using github

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -84,6 +84,12 @@ const submit = () => {
                 >
                     Forgot your password?
                 </Link>
+                <Link
+                    :href="route('login.github')"
+                    class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >
+                    Login Using Github
+                </Link>
 
                 <PrimaryButton class="ms-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
                     Log in
